6550737b3c16bcbf8c9a9ad7d779383aabe5c794
HARISUNDARRAJENDRAN <hsundar080506@gmail.com>
2026-02-12 05:42:37 +0530

    Competed: user auth part consisting of config, authcontrooler and userModel Scheme

--- frontend/src/content/AppContent.jsx
+++ frontend/src/content/AppContent.jsx
@@ -1,3 +1,5 @@
+// This file is used to create a context for the entire application. It can be used to store global state and functions that can be accessed by any component in the application.
+
 import { createContext } from "react";
 
 export const AppContent = createContext();
--- frontend/src/pages/prof/Prof.jsx
+++ frontend/src/pages/prof/Prof.jsx
@@ -4,6 +4,9 @@ const Prof = () => {
     return (
         <div>
             <h1>Professor page</h1>
+            <div>
+                {<Outlet/>}
+            </div>
         </div>
     )
 }
